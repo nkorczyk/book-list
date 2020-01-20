ae59ae452cbf4e3a3126974a3402841fe996bcb5
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-01-20 19:35:38 +0100

    Reading List Project (part 3)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import BookContextsProvider from './contexts/BookContexts';
+
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
       <BookContextsProvider>
         <Navbar />
-        <BookList/>
+        <BookList />
+        <BookForm />
       </BookContextsProvider>
     </div>
   );
